c84fe47ecd147e310fff2ba3b10a78f4d51b2308
antonioalmeida <theantonioalmeida@gmail.com>
2020-12-02 14:57:41 +0100

    add day2 part2

--- day2/index.ts
+++ day2/index.ts
@@ -2,13 +2,11 @@ const fs = require('fs');
 
 const arr = fs.readFileSync('input1.txt').toString().split("\n").map((entry) => {
     const parts = entry.split(':') 
-
     const [min,rest] = parts[0].split('-')
     const [max, char] = rest.split(' ')
-
     const password = parts[1].trim()
 
-    return { min, max, char, password }
+    return { min: parseInt(min), max: parseInt(max), char, password }
 })
 
 const part1 = () => {
@@ -26,4 +24,20 @@ const part1 = () => {
     return valids.reduce((a,b) => a+b)
 }
 
+const part2 = () => {
+
+    const valids = arr.map((entry) => {
+        let nOccurrences = 0
+        if (entry.password[entry.min-1] == entry.char)
+            nOccurrences++
+        if (entry.password[entry.max-1] == entry.char)
+            nOccurrences++
+        
+        return nOccurrences == 1
+    })
+
+    return valids.reduce((a,b) => a+b)
+}
+
 console.log('Part 1:', part1())
+console.log('Part 2:', part2())
