687fb81703a5faa0a280ec06627b79407334f7ba
antonioalmeida <theantonioalmeida@gmail.com>
2020-12-12 23:12:16 +0100

    cleanup day 12

--- src/day12/index.ts
+++ src/day12/index.ts
@@ -6,6 +6,12 @@ function degToRad(degrees) {
 
 export const part1 = () => {
 
+    const rotate = (state, arg) => {
+        let angle = (angles[state.dir] + arg) % 360
+        angle += angle < 0 ? 360 : 0
+        state.dir = angles[angle]
+    }
+
     const instructions = readInput('day12.in').map((entry) => {
         return { op: entry[0], value: parseInt(entry.slice(1)) }
     })
@@ -13,14 +19,8 @@ export const part1 = () => {
     let state = { x: 0, y: 0, dir: 'E' }
 
     const angles = {
-        0: 'E',
-        90: 'S',
-        180: 'W',
-        270: 'N',
-        'E': 0,
-        'S': 90,
-        'W': 180,
-        'N': 270
+        0: 'E', 90: 'S', 180: 'W', 270: 'N',
+        'E': 0, 'S': 90, 'W': 180, 'N': 270
     }
     
     const operations = {
@@ -28,45 +28,28 @@ export const part1 = () => {
         'S': ((state, arg) => { state.y -= arg }),
         'E': ((state, arg) => { state.x += arg }),
         'W': ((state, arg) => { state.x -= arg }),
-        'R': ((state, arg) => {
-            let finalAngle = (angles[state.dir] + arg) % 360
-            finalAngle += finalAngle < 0 ? 360 : 0
-            state.dir = angles[finalAngle]
-        }),
-        'L': ((state, arg) => {
-            let finalAngle = (angles[state.dir] - arg) % 360
-            finalAngle += finalAngle < 0 ? 360 : 0
-            state.dir = angles[finalAngle]
-        })
+        'R': ((state, arg) => { rotate(state, arg) }),
+        'L': ((state, arg) => { rotate(state, -arg) })
     }
 
-    for (const {op, value} of instructions) {
-
-        console.log(op, state)
-        if(op == 'F') {
-            console.log(state.dir)
+    for (const {op, value} of instructions) 
+        if(op == 'F') 
             operations[state.dir](state, value)
-        }
         else 
             operations[op](state, value)
-    }
 
-    return {
-        ...state,
-        res: Math.abs(state.x)+Math.abs(state.y)
-    }
-}
-
-
-const rotate = (state, arg) => {
-    const { wpx, wpy } = state
-    const cos = Math.round(Math.cos(degToRad(arg)))
-    const sin = Math.round(Math.sin(degToRad(arg)))
-    state.wpx = wpx * cos - wpy * sin
-    state.wpy = wpx * sin + wpy * cos
+    return { ...state, res: Math.abs(state.x)+Math.abs(state.y) }
 }
 
 export const part2 = () => {
+
+    const rotate = (state, arg) => {
+        const { wpx, wpy } = state
+        const cos = Math.round(Math.cos(degToRad(arg)))
+        const sin = Math.round(Math.sin(degToRad(arg)))
+        state.wpx = wpx * cos - wpy * sin
+        state.wpy = wpx * sin + wpy * cos
+    }
 
     const instructions = readInput('day12.in').map((entry) => {
         return { op: entry[0], value: parseInt(entry.slice(1)) }
