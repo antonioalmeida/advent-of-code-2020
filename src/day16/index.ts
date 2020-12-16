f83d147b4b586542d30d14ea3c10dd6e68421341
antonioalmeida <theantonioalmeida@gmail.com>
2020-12-16 20:00:29 +0000

    day16 part1 cleanup

--- src/day16/index.ts
+++ src/day16/index.ts
@@ -3,8 +3,7 @@ import { readInput } from '../utils'
 const min = 0
 const max = 1
 
-export const part1 = () => {
-
+const getInput = () => {
     const [rawRules, rawTicket, rawNearby] = readInput('day16.in', '\n\n')
 
     const rules = rawRules.split('\n').map((line) => {
@@ -15,29 +14,33 @@ export const part1 = () => {
 
     const ticket = rawTicket.split('\n')[1].split(',').map((e) => parseInt(e))
     const nearby = rawNearby.split('\n').slice(1).map((line) => line.split(',').map((e) => parseInt(e)))
+    
+
+    return {rules, ticket, nearby}
+}
+
+export const part1 = () => {
+
+    const {rules, ticket, nearby} = getInput()
 
     const wrong:Array<number> = []
 
     for (const t of nearby) {
-        //console.log('Ticket', t)
-        const left = t.filter((field) => { 
-            const matched = rules.map(({rule0, rule1}) => (field >= rule0[min] && field <= rule0[max]) || (field >= rule1[min] && field <= rule1[max]) )
-
-            // @ts-ignore
-            return matched.reduce((a,b) => a+b) == 0
-            //console.log(matched)
-    })
+        // @ts-ignore
+        const left = t.filter((field) => rules.map((
+            {rule0, rule1}) => (field >= rule0[min] && field <= rule0[max]) || (field >= rule1[min] && field <= rule1[max]) 
+        )
+        // @ts-ignore
+        .reduce((a,b) => a+b) == 0)
 
         wrong.push(...left)
     }
 
     return wrong.reduce((a,b) => a+b)
-
-    console.log(rules)
-    console.log(ticket)
-    console.log(nearby)
 }
 
 export const part2 = () => {
-    console.log('Done in Python. Needs rewrite.')
+    const {rules, ticket, nearby} = getInput()
+
+    return 0
 }
