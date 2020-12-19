e169e0e61d9dc49eecc9af23e49971f810929bec
antonioalmeida <theantonioalmeida@gmail.com>
2020-12-19 18:31:18 +0000

    day17 part1

---
+++ src/day17/index.ts
@@ -0,0 +1,102 @@
+import { readInput } from '../utils'
+
+const ACTIVE = '#'
+const INACTIVE = '.'
+
+class State {
+    public x: number
+    public y: number
+    public z: number
+    public active: boolean
+
+    constructor(x: number, y: number, z: number) {
+        this.x = x
+        this.y = y
+        this.z = z
+    }
+
+    static str = (state: State) => JSON.stringify(({ x: state.x, y: state.y, z: state.z }))
+
+    static getNeighbors = (str: string): Map<State, boolean> => {
+
+        const s = JSON.parse(str)
+
+        const neighbors = new Map()
+
+        for (let x of [-1, 0, 1]) {
+            for (let y of [-1, 0, 1]) {
+                for (let z of [-1, 0, 1]) {
+                    if (!(x == 0 && y == 0 && z == 0)) {
+                        const state = new State(s.x + x, s.y + y, s.z + z)
+                        let active = false
+                        if (states.get(State.str(state)))
+                            active = true
+
+                        neighbors.set(state, active)
+                    }
+                }
+            }
+        }
+
+        return neighbors
+    }
+
+}
+
+let states: Map<string, boolean> = new Map()
+
+export const part1 = () => {
+
+    readInput('day17.in').map((line, x) => line.split('').map((state, y) => states.set(State.str(new State(x, y, 0)), state == ACTIVE)))
+
+    let turn = 0
+    while (turn < 6) {
+        const newStates: Map<string, boolean> = new Map()
+        const nextStates: Map<string, boolean> = new Map()
+        console.log('i', turn)
+
+        for (let [stateStr, active] of states.entries()) {
+            const state: State = JSON.parse(stateStr)
+            const neighbors = State.getNeighbors(stateStr)
+
+            for (const [s,a] of neighbors.entries()) 
+                newStates.set(State.str(s), a)
+
+            newStates.set(State.str(state), active)
+        }
+
+        for (let [stateStr, active] of newStates.entries()) {
+
+            const state: State = JSON.parse(stateStr)
+            const neighbors = State.getNeighbors(stateStr)
+
+            let nActive = 0
+            for (const active of neighbors.values())
+                nActive += active ? 1 : 0
+
+            if (active) {
+                let s = false
+                if (nActive == 2 || nActive == 3)
+                    s = true
+
+                nextStates.set(State.str(new State(state.x, state.y, state.z)), s)
+            }
+
+            if (!active && nActive == 3)
+                nextStates.set(State.str(new State(state.x, state.y, state.z)), true)
+        }
+
+        turn++
+        states = nextStates
+    }
+
+    let nActive = 0
+    for (const s of states.values())
+        nActive += s ? 1 : 0
+    
+    return nActive
+}
+
+export const part2 = () => {
+    console.log('Done in Python. Needs rewrite.')
+}
--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,9 @@ import * as day13 from './day13'
 import * as day14 from './day14'
 import * as day15 from './day15'
 import * as day16 from './day16'
+import * as day17 from './day17'
 
-const days = { day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11, day12, day13, day14, day15, day16 }
+const days = { day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11, day12, day13, day14, day15, day16, day17 }
 
 import yargs from 'yargs' 
 import { hideBin } from 'yargs/helpers'
