da6624e17fac9184316cfdf9c9b09fb94cc901b7
antonioalmeida <theantonioalmeida@gmail.com>
2020-12-21 02:40:15 +0000

    day18 part 2

--- src/day18/index.ts
+++ src/day18/index.ts
@@ -10,12 +10,6 @@ class Node {
         this.right = right
         this.op = op
     }
-
-    public str(delim:string = '') {
-        console.log(delim, 'left:', typeof this.left == 'object' ? this.left.str(delim + '    ') : this.left)
-        console.log(delim, 'op:', this.op)
-        console.log(delim, 'right:', typeof this.right == 'object' ? this.right.str(delim + '    ') : this.right)
-    }
 }
 
 const closerFinder = (expression: string[]) => {
@@ -24,31 +18,34 @@ const closerFinder = (expression: string[]) => {
     
     for (i; i > 0; i--) {
         const token = expression[i]  
-        if (token == ')') 
-            n++
-        
+        if (token == ')') n++
         if (token == '(') {
-            if(n == 0)
-                return i
-            else 
-                n--
+            if(n == 0) return i
+            else n--
         }
     }
 }
 
-const parse = (expression: string) => {
+const starFinder = (expression: string[]) => {
+    let i = expression.length - 1
+    let n = 0
     
-    if (expression.length == 0)
-        return
+    for (i; i > 0; i--) {
+        const token = expression[i]  
+        if (token == ')') n++
+        if (token == '(') n--
+        if (token == '*' && n == 0) return i
+    }
 
+    return -1
+}
+
+const parse = (expression: string) => {
+    if (expression.length == 0) return
     const tokens = expression.split('').filter((e) => e != ' ')
-
     let i = tokens.length - 1
     const token = tokens[i]
-
-    let left
-    let right
-    let op
+    let left; let right; let op;
 
     if (token == ')') {
         const j = closerFinder(tokens)
@@ -68,32 +65,54 @@ const parse = (expression: string) => {
     return new Node(left, right, op)
 }
 
+const parse2 = (expression: string) => {
+    if (expression.length == 0) return
+    const tokens = expression.split('').filter((e) => e != ' ')
+    let i = tokens.length - 1
+    const token = tokens[i]
+    let left; let right; let op;
+
+    if (starFinder(tokens) > 0) {
+        const j = starFinder(tokens)
+        const rightExpr = tokens.slice(j+1).join('')
+        i = j+1
+        right = parse2(rightExpr)
+    }
+    else if (token == ')') {
+        const j = closerFinder(tokens)
+        const rightExpr = tokens.slice(j+1, i).join('')
+        i = j
+        right = parse2(rightExpr)
+    }
+    else 
+        right = parseInt(token)
+
+    op = tokens[i - 1]
+    if (tokens[i-3]) 
+        left = parse2(tokens.slice(0, i - 1).join(''))
+    else 
+        left = parseInt(tokens[i-2])
+
+    return new Node(left, right, op)
+}
+
 const op = {
     '+': (a,b) => a+b,
     '*': (a,b) => a*b
 }
 
 const evaluate = (node: Node | number) => {
-
-    if (typeof node == 'number') {
-        return node
-    }
-
-    if (!node.left)
-        return evaluate(node.right)
-
-    const res = op[node.op](evaluate(node.left), evaluate(node.right))
-
-    return res
+    if (typeof node == 'number') return node
+    if (!node.left) return evaluate(node.right)
+    return op[node.op](evaluate(node.left), evaluate(node.right))
 }
 
 export const part1 = () => {
     const ops = readInput('day18.in')
-
     return ops.map((e) => evaluate(parse(e))).reduce((a,b) => a+b)
 }
 
 export const part2 = () => {
-    console.log('Done in Python. Needs rewrite.')
-
+    const ops = readInput('day18.in')
+    return ops.map((e) => evaluate(parse2(e))).reduce((a,b) => a+b)
 }
