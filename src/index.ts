ec2900f8f6287f6f3659921f7bc125b55538021b
antonioalmeida <theantonioalmeida@gmail.com>
2020-12-21 01:07:53 +0000

    day18 part 1

--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,9 @@ import * as day14 from './day14'
 import * as day15 from './day15'
 import * as day16 from './day16'
 import * as day17 from './day17'
+import * as day18 from './day18'
 
-const days = { day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11, day12, day13, day14, day15, day16, day17 }
+const days = { day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11, day12, day13, day14, day15, day16, day17, day18 }
 
 import yargs from 'yargs' 
 import { hideBin } from 'yargs/helpers'
