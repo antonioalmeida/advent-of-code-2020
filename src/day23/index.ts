75e29caf89cdfdd4ee2313d143a54c3215f7a390
antonioalmeida <theantonioalmeida@gmail.com>
2020-12-24 05:13:18 +0000

    day 23 part2 (needs refactor)

--- src/day23/index.ts
+++ src/day23/index.ts
@@ -1,14 +1,11 @@
 import { readInput } from '../utils'
 
-const MAX_MOVES = 100
-
 const pickCups = (arr: number[], current:number) => {
     if (current + 3 < arr.length) 
         return arr.splice(current+1, 3)
     
     const nBack = arr.length - current - 1
     const nFront = 3 - nBack
-
     return [...arr.splice(current+1, nBack), ...arr.splice(0, nFront)]
 }
 
@@ -17,6 +14,7 @@ const addToDestination = (arr: number[], items: number[], index: number) => {
 }
 
 export const part1 = () => {
+    const MAX_MOVES = 100
     const circle = readInput('day23.in', '').map((e) => parseInt(e))
 
     let n = 0
@@ -25,27 +23,17 @@ export const part1 = () => {
     const min = Math.min(...circle)
 
     while (n < MAX_MOVES) {
-        console.log('move', n+1)
-        console.log('cups', circle)
-
         const current = circle[currentIndex]
-        console.log('current', current)
         const removed = pickCups(circle, currentIndex)        
-        console.log('pick up', removed)
 
         let destination = current - 1 >= min ? current-1 : max 
-        while (removed.includes(destination)) 
-            destination = destination > min ? destination-1 : max
-        console.log('destination', destination)
+        while (removed.includes(destination)) destination = destination > min ? destination-1 : max
 
         const destinationIndex = circle.indexOf(destination)
-        console.log('destination index', destinationIndex)
-        console.log('cups', circle)
 
         addToDestination(circle, removed, destinationIndex)
         n++
         let nextCurrent = currentIndex
-        console.log('nextcurrent', nextCurrent)
 
         if (currentIndex > destinationIndex) nextCurrent += 3
         nextCurrent = (nextCurrent + 1) % circle.length
@@ -53,13 +41,47 @@ export const part1 = () => {
         if (nextCurrent < currentIndex) 
             nextCurrent = nextCurrent - 3 >= 0 ? nextCurrent-3 : 0
         currentIndex = nextCurrent
-        console.log('currentindex', currentIndex)
-        console.log('')
     }
 
-    console.log(circle)
+    return circle.join('')
 }
 
 export const part2 = () => {
-    console.log('Done in Python. Needs rewrite.')
+    const MAX_MOVES = 10000000 
+    const labels = readInput('day23.in', '').map((e) => parseInt(e))
+    const labelToNext:Map<number, number> = new Map()
+    let n = 0
+
+    for (let i = 9; i <= 1000000; i++) labels.push(i)
+    for (let i = 1; i <= labels.length; i++) labelToNext.set(labels[i-1], labels[i % labels.length])
+
+    const min = 1
+    let max = 0
+    for (const l of labels) if (l > max) max = l
+
+    let currentLabel = labels[0]
+    while (n < MAX_MOVES) {
+        const nextLabel = labelToNext.get(currentLabel)
+        const next2Label = labelToNext.get(nextLabel)
+        const next3Label = labelToNext.get(next2Label)
+        const nextCurrent = labelToNext.get(next3Label)
+        const removed = [nextLabel, next2Label, next3Label]
+
+        let destination = currentLabel - 1 >= min ? currentLabel-1 : max 
+        while (removed.includes(destination)) destination = destination > min ? destination-1 : max
+
+        const afterDestination = labelToNext.get(destination)
+        labelToNext.set(currentLabel, nextCurrent)
+        labelToNext.set(destination, nextLabel)
+        labelToNext.set(nextLabel, next2Label)
+        labelToNext.set(next2Label, next3Label)
+        labelToNext.set(next3Label, afterDestination)
+
+        n++
+        currentLabel = nextCurrent
+    }
+
+    const one = labelToNext.get(1)
+    const next = labelToNext.get(one)
+    return one*next
 }
